9089ff50ba60ec578610a229f7861bb25f543805
Tyson <mellowt@gmail.com>
2016-12-14 12:56:38 -0700

    began exercise, still buggy

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.currentPage;
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    var promise = mainService.getusers($scope.currentPage);
+
+    promise.then(function(response) {
+      $scope.users = response;
+    });
   }
 
+  $scope.prev = function() {
+    if ($scope.currentPage > 1) {
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+  };
+  
+  $scope.next = function() {
+    if ($scope.total_pages || $scope.total_pages > $scope.currentPage) {
+      $scope.currentPage++;
+      $scope.getUsers();
+    }
+  };
 
   $scope.getUsers();
-
+  
 });
---
+++ notes.md
@@ -0,0 +1,12 @@
+free api's to use:
+  SWAPI.code - star wars api
+  pokeapi.co - pokemon api
+  
+  trail and hiking api
+  twitter and facebook api
+  
+  google free api's
+  
+  github has an api
+  
+  
